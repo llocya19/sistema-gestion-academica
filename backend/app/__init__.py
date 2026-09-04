9a0ecca25b2760805c5fc0e4b2ba5b77c65297a1
llocya <llocyasaraviajuan@gmail.com>
2026-09-04 00:11:33 -0500

    feat: configurar arquitectura base flask

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,34 @@
 from flask import Flask
 
+from app.config.settings import Config
+
+from app.extensions.database import db
+from app.extensions.jwt import jwt
+from app.extensions.cors import cors
+
+
 def create_app():
 
     app = Flask(__name__)
 
+    app.config.from_object(Config)
+
+    # Inicializar extensiones
+
+    db.init_app(app)
+
+    jwt.init_app(app)
+
+    cors.init_app(app)
+
+
     @app.route("/")
     def home():
+
         return {
-            "message": "API Sistema Gestión Académica funcionando"
+            "message": "API Sistema Gestión Académica funcionando",
+            "status": "success"
         }
+
 
     return app
