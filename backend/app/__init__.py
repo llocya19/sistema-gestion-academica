ff0275af549dbd931e734aeb0e92abf7fd78bda3
llocya <llocyasaraviajuan@gmail.com>
2026-09-03 23:54:54 -0500

    feat: crear aplicacion base Flask

---
+++ backend/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+def create_app():
+
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return {
+            "message": "API Sistema Gestión Académica funcionando"
+        }
+
+    return app
